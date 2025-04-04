02822b3f82061b05858dc1def2519a7d4f9f786a
Drew Kerner <drew.kerner@agvance.net>
2025-04-04 16:07:46 -0500

    Add validator and fixing null error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +8,28 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf
   ]
 })
 export class AppComponent {
   public temperatureFormGroup = new FormGroup({
-    fahrenheitInput: new FormControl<number>(32, { nonNullable: true }),
-    celsiusInput: new FormControl<number>(0.0, { nonNullable: true }),
+    fahrenheitInput: new FormControl<number>(32, { nonNullable: true, validators: [Validators.required] }),
+    celsiusInput: new FormControl<number>(0, { nonNullable: true, validators: [Validators.required] }),
 });
 
   fahrenheitToCelsius() {
-    const celsiusRounded = Math.round((this.temperatureFormGroup.controls.fahrenheitInput.value - 32) * (5/9) * 10) / 10;
+    const fahrenheit = Number(this.temperatureFormGroup.controls.fahrenheitInput.value);
+    const celsiusCalculated = (fahrenheit - 32) * (5/9);
+    const celsiusRounded = Math.round(celsiusCalculated * 10) / 10;
     this.temperatureFormGroup.controls.celsiusInput.patchValue(celsiusRounded);
+    this.temperatureFormGroup.controls.fahrenheitInput.patchValue(fahrenheit);
   }
 
   celsiusToFahrenheit() {
-    const fahrenheitRounded = Math.round(this.temperatureFormGroup.controls.celsiusInput.value * (9/5) + 32);
+    const celsius = Number(this.temperatureFormGroup.controls.celsiusInput.value);
+    const fahrenheitRounded = Math.round(celsius * (9/5) + 32);
     this.temperatureFormGroup.controls.fahrenheitInput.patchValue(fahrenheitRounded);
+    this.temperatureFormGroup.controls.celsiusInput.patchValue(celsius);
   }
 }
