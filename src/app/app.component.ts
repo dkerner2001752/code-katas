74ecb785c525e337af1dcb03697d8bd3e98c35ee
Drew Kerner <drew.kerner@agvance.net>
2025-04-04 14:57:45 -0500

    Added functionality to Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: []
+  imports: [
+    ReactiveFormsModule
+  ]
 })
 export class AppComponent {
-  title = 'Temperature Calculator';
+  public temperatureFormGroup = new FormGroup({
+    fahrenheitInput: new FormControl<number>(32, { nonNullable: true }),
+    celsiusInput: new FormControl<number>(0.0, { nonNullable: true }),
+});
 
-  public fahrenheit = 32;
-  public celsius = 0.0;
+  fahrenheitToCelsius() {
+    const celsiusRounded = Math.round((this.temperatureFormGroup.controls.fahrenheitInput.value - 32) * (5/9) * 10) / 10;
+    this.temperatureFormGroup.controls.celsiusInput.patchValue(celsiusRounded);
+  }
+
+  celsiusToFahrenheit() {
+    const fahrenheitRounded = Math.round(this.temperatureFormGroup.controls.celsiusInput.value * (9/5) + 32);
+    this.temperatureFormGroup.controls.fahrenheitInput.patchValue(fahrenheitRounded);
+  }
 }
